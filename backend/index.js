431d850f8b358bdc3bc306f72f7b597f8c3eea5d
vibhu gupta <vibhugupta022@gmail.com>
2025-09-09 12:14:51 +0530

    full folder structure done

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,28 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import router from "./routes/Authroutes.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+
 app.use(express.json());
 app.use(cors());
 
-// Routes
+app.use("/api/auth", router);
+
 app.get("/", (req, res) => {
   res.json({ message: "✅ Server is working" });
 });
 
-// Global error handler (optional but good for backend apps)
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
 });
+
 
 // Start server
 app.listen(PORT, () => {
