92f99db6bb322e727719533160ec9837f4ded0f6
Prakhar <prakharagarwal17jul2006@gmail.com>
2025-09-15 23:46:40 +0530

    Home page completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import emailjs from "@emailjs/browser";
 
 // Initialize EmailJS with your public key
-emailjs.init("kbcjoXlncAcEsc2EC"); // Replace with your EmailJS public key
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
